0130caad40783a3b737c3329dede2b9245f97199
Vretu-Dev <jakub.spieczny1@gmail.com>
2025-01-13 14:52:07 +0100

    V bump

--- CaveiraPistol/Main.cs
+++ CaveiraPistol/Main.cs
@@ -10,7 +10,7 @@ namespace CaveiraPistol
         public override string Author => "Vretu";
         public override string Prefix { get; } = "Caveira";
         public override Version RequiredExiledVersion { get; } = new Version(9, 0, 0);
-        public override Version Version => new Version(1, 0, 1);
+        public override Version Version => new Version(1, 0, 2);
         public static Main Instance { get; private set; }
 
         public override void OnEnabled()

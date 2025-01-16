7259aeaeb9771d7faa609496f1f8ae77f5d9f3a5
Vretu-Dev <jakub.spieczny1@gmail.com>
2025-01-16 20:43:56 +0100

    Uncomment

--- CaveiraPistol/Pistol.cs
+++ CaveiraPistol/Pistol.cs
@@ -157,8 +157,8 @@ namespace CaveiraPistol
             if (!Check(ev.Player.CurrentItem))
                 return;
 
-            //if (ev.Attacker != ev.Player)
-            //{
+            if (ev.Attacker != ev.Player)
+            {
                 if (ev.DamageHandler.Type == DamageType.Firearm || ev.DamageHandler.Type == DamageType.Explosion)
                     {
                         if (!Main.Instance.Config.Scp207 && ev.Player.IsEffectActive<Scp207>())
@@ -181,7 +181,7 @@ namespace CaveiraPistol
                         effectWindows[ev.Player] = Timing.RunCoroutine(EffectWindow(ev.Player));
                     }
                 }
-            //}
+            }
         }
         private IEnumerator<float> EffectWindow(Player player)
         {
